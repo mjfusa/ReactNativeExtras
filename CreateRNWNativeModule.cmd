3bfc14e11791a829ff045574df1a468dd0f24a5b
Mike Francis <mike.francis@live.com>
2020-07-16 13:04:14 -0700

    added note about winrtcpp vsix install

--- CreateRNWNativeModule.cmd
+++ CreateRNWNativeModule.cmd
@@ -1,13 +1,13 @@
 if "%1"=="" goto error
-set RN_VERSION=^0.61.0
-set RN_VERSION_INIT=^0.61
+if "%RN_VERSION%"=="" set RN_VERSION=^^0.62.2
+if "%RN_VERSION_INIT%"=="" set RN_VERSION_INIT=^^0.62
 :start
 cmd /c yarn global add create-react-native-module
-cmd /c create-react-native-module %1
+cmd /c npx create-react-native-module %1
 
 :windows
 cd react-native-%1
-cmd /c yarn add react-native@%RN_VERSION% --dev
+cmd /c yarn add react-native@%RN_VERSION_INIT% --dev
 cmd /c yarn add react-native-windows@%RN_VERSION_INIT% --peer
 goto end
 
--- installtools.cmd
+++ installtools.cmd
@@ -1,10 +1,9 @@
+@echo off
 :checkPrivileges 
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' ( goto continue 
 ) else ( powershell "saps -filepath %0 %1 -verb runas" >nul 2>&1)
 exit /b
-
-
 
 :continue
 if NOT "%1"=="" goto %1
@@ -17,17 +16,30 @@ choco feature enable -n=allowGlobalConfirmation
 for %%a in (git.install, netfx-4.6.1-devpack) do choco install %%a -y
 
 :installwindowssdk
-choco install windows-sdk-10-version-1809-all -y
+rem Windows 10 18362
+choco install windows-sdk-10-version-1903-all -y
+if NOT "%1"=="" goto end
+
 
 :installvs
 REM choco install visualstudio2019community --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+winget install -e Microsoft.VisualStudio.Community
+for %%b in (Microsoft.VisualStudioCode, Microsoft.WindowsTerminal) do winget install -e %%b
+echo "NOTE: For C++ dev, also install C++/WinRT Visual Studio Extension. https://marketplace.visualstudio.com/items?itemName=CppWinRTTeam.cppwinrt101804264"
+if NOT "%1"=="" goto end
 
 :installnode
 choco install nodejs.install --version=12.9.1 -y
 choco install yarn -y
+if NOT "%1"=="" goto end
 
 :installrn
 "%ProgramFiles(x86)%\Yarn\bin\yarn" add global react-native-cli
+if NOT "%1"=="" goto end
+
+:installChrome
+winget install -e Google.Chrome
+if NOT "%1"=="" goto end
 
 goto end
 
